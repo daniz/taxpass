c58251927b80b7f5aeb1f93d6c1684a78b1455eb
daniz <dani.silberstein@gmail.com>
2014-05-31 18:21:14 +0300

    added devise related stuff to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,27 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :configure_devise_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(r)
+    new_request_path
+  end
+
+  protected
+
+  def configure_devise_permitted_parameters
+    registration_params = [:name, :email, :password, :password_confirmation, :phone]
+
+    if params[:action] == 'update'
+      devise_parameter_sanitizer.for(:account_update) { 
+        |u| u.permit(registration_params << :current_password)
+      }
+    elsif params[:action] == 'create'
+      devise_parameter_sanitizer.for(:sign_up) { 
+        |u| u.permit(registration_params) 
+      }
+    end
+  end
+
 end
